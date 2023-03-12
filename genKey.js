a66d1723761c02a6bbd7beca84c911ade0bb245c
unknown <adamborcany@gmail.com>
2023-03-12 12:50:11 +0100

    Fixed genKey

--- genKey.js
+++ genKey.js
@@ -6,7 +6,7 @@ const keypair = web3.Keypair.generate();
 const address = keypair.publicKey.toBase58();
 
 fs.appendFileSync(".env",
-    "SOL_PRIVKEY="+Buffer.from(keypair.secretKey).toString("hex")+"\n"+
-    "SOL_ADDRESS="+address+"\n");
+    "SOL_PRIVKEY=\""+Buffer.from(keypair.secretKey).toString("hex")+"\"\n"+
+    "SOL_ADDRESS=\""+address+"\"\n");
 
 console.log("Generated address: "+address);
